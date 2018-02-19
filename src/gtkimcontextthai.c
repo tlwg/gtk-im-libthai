ab61c82012ed3158becb7936a345f34704ac4ecb
Theppitak Karoonboonyanan <theppitak@gmail.com>
2018-02-19 11:56:27 +0700

    Update my e-mail address.
    * AUTHORS:
    * configure.ac:
    * src/gtkimcontextthai.h:
    * src/gtkimcontextthai.c:
    * src/imthai-libthai.c:
      - Replace my LTN e-mail address with the gmail one.

--- src/gtkimcontextthai.c
+++ src/gtkimcontextthai.c
@@ -15,7 +15,7 @@
  * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  *
- * Author:  Theppitak Karoonboonyanan <thep@linux.thai.net>
+ * Author:  Theppitak Karoonboonyanan <theppitak@gmail.com>
  *
  */
 
